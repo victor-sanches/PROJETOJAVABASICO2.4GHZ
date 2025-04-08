225f5cc193be5c5f1d1c0247eed6d7fcff30c30d
joao.ferreira <joaovictrsanches0902@gmail.com>
2025-04-08 00:29:36 -0400

    Classes das anatomias

---
+++ Meu primeiro projeto-java/src/MinhaClasse.java
@@ -0,0 +1,11 @@
+public class MinhaClasse {
+    public static void main(String [] args) {
+        int ano = 2002;
+        ano = 2025;
+
+        final String BR = "Brasil";
+
+        BR = "Brazil";
+
+    }
+}
